8df1d7b19e4a54e6fc4f99d983d1a6852332d717
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2023-01-20 18:10:00 +0100

    [FT] Delete requests: now the server handles deletions of files.extension and folders

--- src/routes/files.js
+++ src/routes/files.js
@@ -34,16 +34,47 @@ router.get('/:filepath?', async (req, res) => {
   } catch(err) {
     return res.status(404).json({
       "Error": "404 Resource not found."
-    })
-
+    }); 
   } 
   
-  res.json({
+  return res.json({
     "root_directory": path.basename(DATA_PATH),
     "content": content, 
     "count": content.files.length + content.directories.length 
   }); 
-}); 
+});
+
+
+router.delete('/:filepath?', async (req, res) => {
+
+  const query = req.params.filepath || ''; // another path || root path
+  const requestPath = getQueryPath(query); 
+
+  try {
+    const isDirectory = fs.lstatSync(requestPath).isDirectory(); 
+
+    if (isDirectory) {
+      fs.rmSync(requestPath, {recursive: true, force: true});
+    } else {
+      fs.unlinkSync(requestPath, (err) => {
+        if (err) {
+          return res.status(500).json({
+            "Error": "500 Internal server error"
+          }); 
+        }
+      }); 
+    }
+  } catch(err) {
+    console.log(err); 
+    return res.status(404).json({
+      "Error": "404 Resource not found."
+    })
+  } 
+  
+  return res.status(200).json({
+    "status": "Deleted"
+  }); 
+});
 
 
 module.exports = router; 
