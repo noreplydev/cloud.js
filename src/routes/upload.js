512068a05f2d0d48fd7cf1616c6e89c321f84bc9
sanchez-cristian <contact.cristiansanchez@gmail.com>
2023-02-18 22:02:42 +0100

    [FT] Upload path: now the upload on a folder is supported

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -3,13 +3,13 @@ const {Router} = require('express');
 const router = Router();
 const path = require('path'); 
 
-const {DATA_PATH} = require('../config.js'); 
+const {DATA_PATH, DIRECTORY_DELIMITER} = require('../config.js'); 
 const enabledUpload = require('../middlewares/enabledUpload.js'); 
 
 // middleware for upload config
 router.use(enabledUpload); 
 
-router.post('/', (req, res) => {
+router.post('/:path', (req, res) => {
   let file; 
 
   if (!req.files || Object.keys(req.files) === 0) {
@@ -19,10 +19,11 @@ router.post('/', (req, res) => {
   }
 
   file = req.files.file; 
-  const FILES_PATH= path.join(DATA_PATH, file.name);
+  
+  const FILE_PATH = getPath(req.params.path, file.name);
 
-  console.log(FILES_PATH);   
-  file.mv(FILES_PATH, (err) => {
+  console.log(FILE_PATH);   
+  file.mv(FILE_PATH, (err) => {
     if (err) {
       console.log('err', err)
       return res.status(500).json({
@@ -35,6 +36,14 @@ router.post('/', (req, res) => {
     }); 
   }); 
 })
+
+function getPath(string, filename) {
+  const separator = process.platform === 'win32' ? '\\' : '/'; 
+
+  const segments = string.split(DIRECTORY_DELIMITER); 
+  const relative = segments.join(separator); 
+  return path.join(DATA_PATH, relative, filename); 
+}
 
 module.exports = router; 
 
